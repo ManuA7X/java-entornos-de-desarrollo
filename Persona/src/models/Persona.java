07050163d655f8872436e510aeec2411310d414e
ManuA7X <44942727+ManuA7X@users.noreply.github.com>
2024-03-14 16:23:33 +0100

    Persona

---
+++ Persona/src/models/Persona.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    public void dni;
+
+    /**
+     * 
+     */
+    private void nombre: String;
+
+    /**
+     * 
+     */
+    private void apellidos: String;
+
+    /**
+     * 
+     */
+    private void edad: int;
+
+    /**
+     * @param dni: String 
+     * @param nombre: String 
+     * @param apellidos: String 
+     * @param edad: int
+     */
+    public void Persona(void dni: String, void nombre: String, void apellidos: String, void edad: int) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el Dni de la persona
+     * @param dni: String
+     */
+    public void setDni(void dni: String) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el Dni de la persona
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre de la persona
+     * @param nombre: String
+     */
+    public void setNombre(void nombre: String) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el nombre de la persona
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica los apellidos de la persona
+     * @param apellidos: String
+     */
+    public void setApellidos(void apellidos: String) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve los apellidos de la persona
+     * @return
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica la edad de la persona
+     * @param edad: int
+     */
+    public void setEdad(void edad: int) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve la edad de la persona
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
---
+++ PruebaGit/src/models/Persona.java
@@ -0,0 +1,112 @@
+package models;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    public String dni;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String apellidos;
+
+    /**
+     * 
+     */
+    private int edad;
+
+    /**
+     * @param dni: String 
+     * @param nombre: String 
+     * @param apellidos: String 
+     * @param edad: int
+     */
+    public Persona(String dni, String nombre, String apellidos, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Modifica el Dni de la persona
+     * @param dni: String
+     */
+    public void setDni(String dni) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el Dni de la persona
+     * @return
+     */
+    public String getDni() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica el nombre de la persona
+     * @param nombre: String
+     */
+    public void setNombre(String nombre) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve el nombre de la persona
+     * @return
+     */
+    public String getNombre() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica los apellidos de la persona
+     * @param apellidos: String
+     */
+    public void setApellidos(String apellidos) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve los apellidos de la persona
+     * @return
+     */
+    public String getApellidos() {
+        // TODO implement here
+        return "";
+    }
+
+    /**
+     * Modifica la edad de la persona
+     * @param edad: int
+     */
+    public void setEdad(int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * Devuelve la edad de la persona
+     * @return
+     */
+    public int getEdad() {
+        // TODO implement here
+        return 0;
+    }
+
+}
