5cc7657bffc5f7127df5cac334403b3eaebb7a54
ManuA7X <44942727+ManuA7X@users.noreply.github.com>
2024-03-19 16:37:15 +0100

    Main subido

--- Figuras/src/app/Main.java
+++ Figuras/src/app/Main.java
@@ -1,10 +1,20 @@
 package app;
 
+import modelos.Rectangulo;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo(1, 4);
+		
+		int area1 = r1.area();
+		int perimetro2 = r2.perimetro();
+		
+		System.out.println("El area del rectangulo 1 es: " + area1);
+		System.out.println("El perimetro del rectangulo 2 es: " + perimetro2);
 	}
 
 }
